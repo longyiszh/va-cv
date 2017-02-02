31b1c485c1fca4247f65e06f715b166091558231
valorad <wcxaaa@gmail.com>
2017-02-02 16:01:55 +0800

    * standardized user data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 
+interface IUser {
+  name: string;
+  avatar?: string;
+  company: string;
+  position: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = `Valorad's Resume`;
-  username = "Valorad de la Septia";
+
+  public user: IUser = {
+    name: "Valorad de la Septia",
+    avatar: "fallout_vaultboy_va.png",
+    company: "Abstergo Entertainment",
+    position: "Project Manager"
+  }
+
+  title = `${ this.user.name }'s Resume for ${ this.user.position } at ${ this.user.company }`;
 }
