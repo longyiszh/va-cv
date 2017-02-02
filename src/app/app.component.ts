5314ebac5e2edb759a5a15aef5995ef6c250729b
valorad <wcxaaa@gmail.com>
2017-02-02 15:35:59 +0800

    + designed cover

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = `Valorad's Resume`;
+  username = "Valorad de la Septia";
 }
