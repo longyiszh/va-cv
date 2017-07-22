2b3150667c4e8af337c1b002550e4f5b87e3caea
Valorad <wcxaaa@gmail.com>
2017-07-22 13:37:21 +0800

    * style recovery

--- src/app/utilModule/md.module.ts
+++ src/app/utilModule/md.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { 
-
+  MdCardModule
 } from '@angular/material';
 
 
 @NgModule({
   imports: [
-
+    MdCardModule
   ],
   exports: [
-
+    MdCardModule
   ]
 })
 export class MdModule { }
